a2d66102394a892fd643dc85c58f349598246935
Garrett Smith <gasmith@uni-potsdam.de>
2024-06-19 14:42:19 +0200

    Add first two chapters.

---
+++ CrowsNest/src/CrowsNest/Program.fs
@@ -0,0 +1,40 @@
+﻿// Ch. 2 of Youens-Clark's *Tiny Python Projects*
+
+// Command line boilerplate
+open Argu
+type CliArguments =
+    | [<MainCommand>] Word of word: string // As positional argument
+    //| [<AltCommandLine("-n")>] Name of name: string
+
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Word _ -> "A word"
+
+// An active pattern to identify vowels
+let (|Vowel|Consonant|) (input: char) =
+    let vowels = ['a'; 'e'; 'i'; 'o'; 'u']
+    if List.contains input vowels then Vowel else Consonant
+
+[<EntryPoint>]
+let main args =
+    // Letting Argu handle incorrect inputs gracefully
+    let errorHandler = ProcessExiter(None)
+
+    // Set up the argument parser
+    let parser =
+        ArgumentParser.Create<CliArguments>(programName = "CrowsNest", errorHandler = errorHandler)
+    
+    // Parse the arguments
+    let result = parser.ParseCommandLine(args)
+    let word = result.TryGetResult Word |> Option.defaultValue "danger"
+
+    // Getting the right determiner
+    let determiner =
+        match (System.Char.ToLower word[0]) with
+        | Vowel -> "an"
+        | Consonant -> "a"
+
+    // Output
+    printfn $"Ahoy, Captain, {determiner} {word} off the larbord bow!"
+    0
---
+++ hello/Program.fs
@@ -0,0 +1,31 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open Argu
+
+type CliArguments =
+    //| [<MainCommand>] Name of name: string // As positional argument
+    | [<AltCommandLine("-n")>] Name of name: string
+
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Name _ -> "Name to greet"
+    
+
+[<EntryPoint>]
+let main args =
+    // Letting Argu handle incorrect inputs gracefully
+    let errorHandler = ProcessExiter(None)
+
+    // Set up the argument parser
+    let parser =
+        ArgumentParser.Create<CliArguments>(programName = "hello", errorHandler = errorHandler)
+    
+    // Parse the arguments
+    let result = parser.ParseCommandLine(args)
+
+    // Getting the name, if wasn't entered, then use "World"
+    let name = result.TryGetResult Name |> Option.defaultValue "World"
+    
+    printfn $"Hello, {name}!"
+    0
