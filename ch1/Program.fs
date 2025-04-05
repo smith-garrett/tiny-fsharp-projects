d6364a387e27664eeb8ae1b82025ece31fef11bf
Garrett Smith <garrettsmith78@gmail.com>
2025-04-05 08:56:11 +0200

    Add Ch. 1 program

---
+++ ch1/Program.fs
@@ -0,0 +1,20 @@
+﻿open Argu
+
+type Arguments =
+    | [<AltCommandLine("-n")>] Name of name: string
+
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Name _ -> "Name to greet"
+
+[<EntryPoint>]
+let main argv =
+    let errorHandler = ProcessExiter(None)
+    let parser = ArgumentParser.Create<Arguments>(programName = "helloWorld", errorHandler=errorHandler)
+
+    let result = parser.ParseCommandLine argv
+    let name = result.GetResult(Name, defaultValue = "world")
+
+    printfn "Hello, %s!" name
+    0
